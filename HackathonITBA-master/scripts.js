260fa7a6b95d02f7bc69dd5c0723b4c04a092635
Nicolas Benenzon <nicolasbenenzon@gmail.com>
2017-11-04 14:19:14 -0300

    Add files via upload

---
+++ HackathonITBA-master/scripts.js
@@ -0,0 +1,18 @@
+$("document").ready ( function() {
+	$(".boti").click( function(){
+		var title = $("#" + $(this).attr("id")+"necesidades").text();
+		console.log(title);
+		document.getElementById("title-for-donation").innerHTML = title;
+
+	});
+
+	$("#notification-opener").click( function(){
+			console.log("entered function");
+			$('#notificacion').modal('show');
+	});
+	$("#medal-opener").click( function(){
+			console.log("entered function");
+			$('#medal-notification').modal('show');
+	});
+
+});
